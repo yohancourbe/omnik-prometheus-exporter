b30d8dc5276c6a688c58e1fb55997ca83b7c9f52
Yohan Courbe <ycourbe@gmail.com>
2023-12-26 22:46:04 +0100

    Update OmnikExport.py

--- app/OmnikExport.py
+++ app/OmnikExport.py
@@ -145,26 +145,26 @@ class OmnikExport:
     @staticmethod
     def generate_string(serial_no):
         """Create request string for inverter.
-
+        
         The request string is built from several parts. The first part is a
         fixed 4 char string; the second part is the reversed hex notation of
         the s/n twice; then again a fixed string of two chars; a checksum of
         the double s/n with an offset; and finally a fixed ending char.
-
+        
         Args:
             serial_no (int): Serial number of the inverter
-
+        
         Returns:
             str: Information request string for inverter
         """
         response = b'\x68\x02\x40\x30'
-
+        
         double_hex = hex(serial_no)[2:] * 2
-        hex_list = [double_hex[i:i + 2].encode('utf-8') for i in
+        hex_list = [int(double_hex[i:i + 2], 16) for i in
                     reversed(range(0, len(double_hex), 2))]
-
-        cs_count = 115 + sum([ord(c) for c in hex_list])
-        checksum = hex(cs_count)[-2:].encode('utf-8')
-        response += b''.join(hex_list) + b'\x01\x00' + checksum + b'\x16'
+        
+        cs_count = 115 + sum(hex_list)
+        checksum = cs_count.to_bytes(1, byteorder='big')
+        response += bytes(hex_list) + b'\x01\x00' + checksum + b'\x16'
+        
         return response
-
